7d39fdb780d465cb8f51a84855f4d193f29e4b3b
wenlongQAQ <lwl773923@outlook.com>
2022-11-08 10:42:29 +0800

    11.8
    最后的功能 真不更新了

--- src/main/java/com/example/softwaresystem/controller/UserController.java
+++ src/main/java/com/example/softwaresystem/controller/UserController.java
@@ -18,6 +18,11 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    /**
+     * 根据名称查询用户
+     * @param user
+     * @return
+     */
     @GetMapping("/search")
     public R<List<User>> searchOne(User user){
         LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
@@ -29,6 +34,10 @@ public class UserController {
         else return R.error("出错了");
     }
 
+    /**
+     * 查询状态码为0的用户 即需要申领车牌的用户
+     * @return
+     */
     @GetMapping("/status")
     public R<List<User>> selectrRequest(){
         LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
@@ -37,6 +46,22 @@ public class UserController {
         return R.success(list,"查询成功");
     }
 
+    /**
+     * 用于显示还有多少条消息尚未处理
+     * @return
+     */
+    @GetMapping("/solution")
+    public R<Integer> countsSolution(){
+        LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(User::getStatus,0);
+        int count = service.count(lambdaQueryWrapper);
+        return R.success(count,"");
+    }
+
+    /**
+     * 查看所有的用户信息
+     * @return
+     */
     @GetMapping
     public R<List<User>> selectAll(){
         LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
@@ -45,11 +70,23 @@ public class UserController {
         return R.success(list,"查询成功");
     }
 
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public R<User> selectAll(@PathVariable Integer id){
         User byId = service.getById(id);
         return R.success(byId,"查询成功");
     }
+
+    /**
+     * 用户登录
+     * @param request
+     * @param user
+     * @return
+     */
     @PostMapping("/login")
     public R<User> userLogin(HttpServletRequest request, @RequestBody User user){
         LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
@@ -63,6 +100,13 @@ public class UserController {
         else
             return R.error("登录失败");
     }
+
+    /**
+     * 新增用户
+     * @param request
+     * @param user
+     * @return
+     */
     @PostMapping("/save")
     public R<String> addUser(HttpServletRequest request,@RequestBody User user){
         if (user.getUsername()==null)
@@ -72,6 +116,13 @@ public class UserController {
             return R.success("","新增成功!");
         }else return R.error("操作失败!");
     }
+
+    /**
+     * 用户申请车牌
+     * @param request
+     * @param user
+     * @return
+     */
     @PostMapping("/request")
     public R<String> sendRequest(HttpServletRequest request,@RequestBody User user){
         if ((Integer) request.getSession().getAttribute("user")==0){
@@ -82,6 +133,12 @@ public class UserController {
             return R.success("","提交成功!!!");
         }else return R.error("操作失败!");
     }
+
+    /**
+     * 更新用户信息
+     * @param user
+     * @return
+     */
     @PutMapping
     public R<String> update(@RequestBody User user){
        if (user.getStatus()==0||user.getStatus()==2){
@@ -95,6 +152,11 @@ public class UserController {
         }
     }
 
+    /**
+     * 驳回用户申请
+     * @param user
+     * @return
+     */
     @PutMapping("/requestRefuse")
     public R<String> refuse(User user){
         System.out.println(user.getId());
@@ -109,6 +171,12 @@ public class UserController {
             return R.error("修改失败");
         }
     }
+
+    /**
+     * 删除用户
+     * @param user
+     * @return
+     */
     @DeleteMapping("/delete")
     public R<String> delete(User user){
         LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
@@ -116,6 +184,12 @@ public class UserController {
         service.remove(queryWrapper);
         return R.success("","删除成功里!");
     }
+
+    /**
+     * 用户登出
+     * @param request
+     * @return
+     */
     @DeleteMapping
     public R<String > logonout(HttpServletRequest request){
         request.getSession().invalidate();
